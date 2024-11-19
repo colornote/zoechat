381c7a26ad4004aa8ed38663339b07974a53d612
colornote <colornote99@gmail.com>
2024-11-19 11:32:21 +0800

    modify title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import '@/styles/theme-config.css'
 
 export const metadata = {
   title: {
-    default: 'ChatGPT Lite',
-    template: `%s - ChatGPT Lite`
+    default: 'ZoeChat',
+    template: `%s - ZoeChat`
   },
-  description: 'AI assistant powered by ChatGPT',
+  description: 'AI assistant powered by ZoeChat',
   icons: {
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
