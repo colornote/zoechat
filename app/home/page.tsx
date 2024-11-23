7ece451d953f359302b5870e1f89a847ed3f1be8
colornote <colornote99@gmail.com>
2024-11-23 20:08:41 +0800

    fixed footer

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
           </div>
         </div>
       </main>
-      <footer className="bg-white">
+      <footer className="bg-white fixed bottom-0 w-full">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <p className="text-center text-gray-500">&copy; 2023 Zoe Chat. All rights reserved.</p>
         </div>
